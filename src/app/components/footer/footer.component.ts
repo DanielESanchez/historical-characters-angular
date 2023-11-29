414f8a420f0db2daaf69c0fe6925298105f6f178
Daniel Sanchez <danielsanczuniga@gmail.com>
2023-11-29 10:42:09 -0600

    Added footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-footer',
+  selector: "app-footer",
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  templateUrl: "./footer.component.html",
+  styleUrl: "./footer.component.scss",
 })
 export class FooterComponent {
-
+  public year = new Date().getFullYear();
 }
